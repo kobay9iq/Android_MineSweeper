f6249e90f96bc9d74b56c227db273292880eec2a
kobay9iq <kby9iq@gmail.com>
2023-10-24 17:12:24 +0300

    vbcv

--- app/src/main/java/com/example/sapper/MainActivity.java
+++ app/src/main/java/com/example/sapper/MainActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.View.OnLongClickListener;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import java.net.CookieHandler;
@@ -13,14 +15,19 @@ import java.net.CookieHandler;
 public class MainActivity extends AppCompatActivity {
 
   Button[][] cells;
+  final int MINESCONST = 3;
 
-  final int WIDTH = 10;
-  final int HEIGHT = 10;
+  int minesCurrent = 3;
+  TextView mines;
+  final int WIDTH = 5;
+  final int HEIGHT = 5;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    mines = findViewById(R.id.mines);
+    mines.setText("" + minesCurrent + " / " + MINESCONST);
     generate();
   }
 
@@ -41,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
     for (int i = 0; i < HEIGHT; i++) {
       for (int j = 0; j < WIDTH; j++) {
-        cells[i][j].setText("" + (j + HEIGHT * i));
+        cells[i][j].setText("" + (j + HEIGHT * i + 1));
         cells[i][j].setTag("" + (j + HEIGHT * i));
         cells[i][j].setOnClickListener(
             new View.OnClickListener() {
@@ -50,13 +57,20 @@ public class MainActivity extends AppCompatActivity {
                 v.setBackgroundColor(Color.RED);
               }
             });
-        cells[i][j].setOnLongClickListener(new OnLongClickListener() {
-          @Override
-          public boolean onLongClick(View v) {
-            v.setBackgroundColor(Color.BLUE);
-            return true;
-          }
-        });
+        cells[i][j].setOnLongClickListener(
+            new OnLongClickListener() {
+              @Override
+              public boolean onLongClick(View v) {
+                v.setBackgroundColor(Color.BLUE);
+                --minesCurrent;
+                mines.setText("" + minesCurrent + " / " + MINESCONST);
+                if (minesCurrent == 0) {
+                  Toast.makeText(getApplicationContext(), "WIN!!11!", Toast.LENGTH_LONG).show();
+                  Runtime.getRuntime().exit(0);
+                }
+                return true;
+              }
+            });
         layout.addView(cells[i][j]);
       }
     }
